0f10470c4e3f1dda7b17cf4f3a5587bb13e09932
ilokhat <lartane@gmail.com>
2018-05-17 18:15:23 +0200

    minor corrections to the new process producing "parcelles" with rules

--- src/main/java/EPFIFTaskRunner.java
+++ src/main/java/EPFIFTaskRunner.java
@@ -1,5 +1,8 @@
 import java.io.File;
 
+import org.apache.commons.lang3.StringUtils;
+
+import fr.ign.cogit.simplu3d.experiments.iauidf.tool.ParcelAttributeTransfert;
 import fr.ign.cogit.simplu3d.experiments.openmole.EPFIFTask;
 import fr.ign.cogit.simplu3d.io.feature.AttribNames;
 
@@ -9,32 +12,43 @@ public class EPFIFTaskRunner {
     EPFIFTask.USE_DEMO_SAMPLER = false;
     EPFIFTask.INTERSECTION = true;
     EPFIFTask.FLOOR_HEIGHT = 3;
-    EPFIFTask.MAX_PARCEL_AREA = 10000;
-    EPFIFTask.PARCEL_NAME = "parcelle_new.shp";
+    EPFIFTask.MAX_PARCEL_AREA = 10000; // now obsolete
+    EPFIFTask.PARCEL_NAME = "parcelle.shp";
+    
+	ParcelAttributeTransfert.att_libelle_zone = "LIBELLE_ZO";
+	ParcelAttributeTransfert.att_art_10_m = "B1_HAUT_M";
+	ParcelAttributeTransfert.att_art_10_m_2 = "B2_HAUT_M";
 
     // String[] folderSplit = folder.getAbsolutePath().split(File.separator);
     String imu = dirName;// folderSplit[folderSplit.length - 1];
 
     String result = "";
+    	 
     try {
-      result = EPFIFTask.run(folder, dirName, folderOut, parameterFile, seed);
+    	result = EPFIFTask.run(folder, dirName, folderOut, parameterFile, seed);
     } catch (Exception e) {
-      result = "# " + imu + "#\n";
-      result += e.toString() + "\n";
-      for (StackTraceElement s : e.getStackTrace())
-        result += "# " + s.toString() + "\n";
-      result = "# " + imu + "#\n";
+      result = "# " + imu + " #\n";
+      result += "# "+ e.toString() + "\n";
+//      for (StackTraceElement s : e.getStackTrace())
+//        result += "# " + s.toString() + "\n";
+      result += "# " + imu + " #\n";
       e.printStackTrace();
     }
     return result;
   }
 
   public static void main(String[] args) {
+	ParcelAttributeTransfert.att_libelle_zone = "LIBELLE_ZO";
+	ParcelAttributeTransfert.att_art_10_m = "B1_HAUT_M";
+	ParcelAttributeTransfert.att_art_10_m_2 = "B2_HAUT_M";
     String numrep = "77054334"; // "77059077";
+    numrep = "78606";
     String foldName = "/home/imran/.openmole/imran-OptiPlex-9010/webui/projects/dataBasicSimu/idf/";
+    foldName = "/home/imran/testoss/dirs2/";
     File folder = new File(foldName + numrep + "/");
     File folderOut = new File("/home/imran/testoss/out/" + numrep + "/");
     File parameterFile = new File("/home/imran/testoss/EPFIF/parameters_iauidf.xml");
+    parameterFile = new File("/home/imran/workspace/simplu3d-openMole-runner/src/main/resources/parameters_iauidf_good.xml");
     long seed = 42L;
     String res = "";
     res = run(folder, numrep, folderOut, parameterFile, seed);
